d307b770c03ed88647f88b6e34986bec7b83f9b5
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-02-23 23:05:26 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react"
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+const getTokenFromUrl = () => {
+  return window.location.hash.substring(1).split("&").reduce((initial, item) => {
+    let parts = item.split("=")
+    initial[parts[0]] = decodeURIComponent(parts[1])
+    return initial;
+  }, {});
+};
+
+function App() {
+  const [spotifyToken, setSpotifyToken] = useState("");
+  const [nowPlaying, setNowPlaying] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // console.log("This is what we derived from the URL: ", getTokenFromUrl())
+    const spotifyToken = getTokenFromUrl().access_token;
+    window.location.hash = ""; 
+    // console.log("This is our spotify token: ", spotifyToken);
+    
+    if (spotifyToken) {
+      setSpotifyToken(spotifyToken);
+      spotifyApi.setAccessToken(spotifyToken)
+      spotifyApi.getMe().then((user) => {
+        // console.log(user);
+      })
+      setLoggedIn(true);
+    }
+
+  })
+
+  const getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response)
+      setNowPlaying({
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url
+      })
+    })
+  }
+
+  const getTopTracks = () => {
+    spotifyApi.getMyTopTracks({
+      time_range: "short_term"
+    }).then((response) => {
+      console.log(response.items[0].name)
+      setNowPlaying({
+        name: response.items[0].name,
+        albumArt: response.items[0].album.images[0].url
+      })
+    })
+
+  }
+
+  const getGenre = () => {
+    spotifyApi.getTrack({
+      trackId: "7tKK8AGJ6Nfefm829JwLgr"
+    }).then((response) => {
+      console.log(response)
+    })
+  }
+
+  return (
+    <div className="App">
+      {!loggedIn && <a href="http://localhost:8888">Login to Spotify</a>}
+      {loggedIn && (
+        <>
+          <div>Now Playing: {nowPlaying.name}</div>
+          <div>
+            <img src={nowPlaying.albumArt} style={{height: 150}}/>
+          </div>
+        </>
+      )}
+      {loggedIn && (
+        <button onClick={() => getGenre()}>Check Now Playing</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
