314050e1428bff3d5febc9a0d79d5a2c149fba0a
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-03-01 23:52:57 -0500

    Get user's top genres
    Accesses user's top tracks, then the respective artist of each track, and then the genre of the artist

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,13 @@ function App() {
   const [spotifyToken, setSpotifyToken] = useState("");
   const [nowPlaying, setNowPlaying] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
+  const [track, setTrack] = useState();
+  const [topTracks, setTopTracks] = useState([]);
+  const [numTracks, setNumTracks] = useState(0);
+  const [artists, setArtists] = useState([]);
+  const [numArtists, setNumArtists] = useState(0);
+  const [genres, setGenres] = useState([]);
+  const [numGenres, setNumGenres] = useState(0);
 
   useEffect(() => {
     // console.log("This is what we derived from the URL: ", getTokenFromUrl())
@@ -35,35 +42,63 @@ function App() {
 
   })
 
-  const getNowPlaying = () => {
-    spotifyApi.getMyCurrentPlaybackState().then((response) => {
-      console.log(response)
-      setNowPlaying({
-        name: response.item.name,
-        albumArt: response.item.album.images[0].url
-      })
-    })
-  }
+  // const getNowPlaying = () => {
+  //   spotifyApi.getMyCurrentPlaybackState().then((response) => {
+  //     console.log(response)
+  //     setNowPlaying({
+  //       name: response.item.name,
+  //       albumArt: response.item.album.images[0].url
+  //     })
+  //   })
 
-  const getTopTracks = () => {
+  //     // setNowPlaying({
+  //     //   name: response.items[0].name,
+  //     //   albumArt: response.items[0].album.images[0].url
+  //     // })
+  // }
+
+  const getTopTracksID = () => {
     spotifyApi.getMyTopTracks({
       time_range: "short_term"
     }).then((response) => {
-      console.log(response.items[0].name)
-      setNowPlaying({
-        name: response.items[0].name,
-        albumArt: response.items[0].album.images[0].url
-      })
+      setNumTracks(response.items.length)
+      let tracksList = topTracks;
+      for (let i = 0; i < numTracks; i++){
+        tracksList.push(response.items[i].id); // song id 
+      }
+      setTopTracks(tracksList);
     })
-
   }
 
-  const getGenre = () => {
-    spotifyApi.getTrack({
-      trackId: "7tKK8AGJ6Nfefm829JwLgr"
-    }).then((response) => {
-      console.log(response)
-    })
+  const getAllArtists = () => {
+    let listenedArtists = artists;
+    for (let i = 0; i < numTracks; i++){
+      spotifyApi.getTrack(topTracks[i]).then((response) => {
+        let artistURI = response.artists[0].uri.split(':')[2];
+        if(listenedArtists.indexOf(artistURI) === -1) {
+          listenedArtists.push(artistURI);
+        }      
+      })
+    }
+    setArtists(listenedArtists);
+    setNumArtists(listenedArtists.length)
+  }
+
+  const getAllGenres = () => {
+    let listenedGenres = genres; 
+    for (let i = 0; i < numArtists; i++){
+      spotifyApi.getArtist(artists[i]).then((response) => {
+        let artistGenres = response.genres;
+        for (let j = 0; j < response.genres.length; j++){
+          let currentGenre = artistGenres[j];
+          if(listenedGenres.indexOf(currentGenre) === -1) {
+            listenedGenres.push(currentGenre);
+          }      
+        }
+      })
+    }
+    setGenres(listenedGenres);
+    console.log(genres);
   }
 
   return (
@@ -73,13 +108,18 @@ function App() {
         <>
           <div>Now Playing: {nowPlaying.name}</div>
           <div>
-            <img src={nowPlaying.albumArt} style={{height: 150}}/>
+            {/* <img src={nowPlaying.albumArt} style={{height: 150}}/> */}
           </div>
         </>
       )}
       {loggedIn && (
-        <button onClick={() => getGenre()}>Check Now Playing</button>
+        <div>
+          <button onClick={() => getTopTracksID()}>Tracks</button>
+          <button onClick={() => getAllArtists()}>Artists</button>
+          <button onClick={() => getAllGenres()}>Genres</button>
+        </div>
       )}
+      {/* <h1>{track.name}</h1> */}
     </div>
   );
 }
