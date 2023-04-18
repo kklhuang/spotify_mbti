e4e2dd07d205fdf82332b7675bb9762167eabca2
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-04-18 17:37:45 -0400

    Updated functionality, added components
    Added Navbar, Homepage, Footer. Fleshed out main content page. Plans to clean up code in feature edits.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react"
 import SpotifyWebApi from "spotify-web-api-js";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route} from "react-dom"
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -19,15 +23,18 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [track, setTrack] = useState();
   const [topTracks, setTopTracks] = useState([]);
-  const [lenTopTracks, setLenTopTracks] = useState(0);
+  // const [lenTopTracks, setLenTopTracks] = useState(0);
   const [numTracks, setNumTracks] = useState(0);
   const [artists, setArtists] = useState([]);
   const [numArtists, setNumArtists] = useState(0);
   const [genres, setGenres] = useState([]);
   const [numGenres, setNumGenres] = useState(0);
   const [numPlaylists, setNumPlaylists] = useState([]);
+  const [playlistsAndGenres, setPlaylistsAndGenres] = useState(0);
   const [topHits, setTopHits] = useState([]);
   const [userID, setUserID] = useState("");
+  const [newPlaylist, setNewPlaylist] = useState("");
+  const [personalityComplete, setPersonalityComplete] = useState(false)
   const oldYearVar = 2003; 
  
 
@@ -36,6 +43,23 @@ function App() {
   const [varietyScore, setVarietyScore] = useState("");
   const [popularityScore, setPopularityScore] = useState("");
   const [timeScore, setTimeScore] = useState("");
+
+  const personalities = {
+	"M": "Minimalist",
+	"D": "Detailed",
+	"L": "Loyal",
+	"E": "Eclectic",
+	"T": "Trendsetter",
+	"B": "Bandwagon",
+	"P": "Presentist",
+	"V": "Vintage"
+  }
+
+  const descriptions = {
+	"TEST" : "Personality description hereLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  }
+
+  const [personality, setPersonality] = useState("");
   const [songsInTopHits, setSongsInTopHits] = useState([])
 
   const topHitsPlaylistId = "37i9dQZF1DXcBWIGoYBM5M";
@@ -56,24 +80,57 @@ function App() {
       setLoggedIn(true); 
     }
     spotifyApi.getMe().then((response) => {
-      console.log(response)
+      setUserID(response.id)
     })
   })
 
-  useEffect(() => {
-    getTopTracksID();
-    getAllArtists();
-    getAllGenres();
-    getPlaylists();
-    fetchTopHitsIDs();
-    numTracksInTopHits();
-    fetchTopTrackDates();
-  }, [spotifyToken, topTracks]);
+//   async function makePersonalityPlaylist() {
+//       const response = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+//         headers: {
+//           'Authorization': `Bearer ${spotifyToken}`
+//         }
+//       }); 
+//   }
 
+  const makePersonalityPlaylist = async () => {
+	const response = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+	  method: 'POST',
+	  headers: {
+		'Authorization': `Bearer ${spotifyToken}`,
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({
+		name: personality + " Playlist",
+		description: "The playlist that created your Spotify Listener's Personality! Made by Karen Huang :)"
+	  })
+	});
+  
+	const data = await response.json();
+	setNewPlaylist(data.id)
+	return data.id; // return the new playlist ID
 
+  };
+
+  const makePlaylist = async () => {
+	var uriArray = []
+	for (let i = 0; i < numTracks; i++) {
+		uriArray.push("spotify:track:" + topTracks[i])
+	}
+	const response = await fetch(`https://api.spotify.com/v1/playlists/${newPlaylist}/tracks`, {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${spotifyToken}`,
+			'Content-Type': 'application/json'
+			},
+		body: JSON.stringify({
+			uris: uriArray
+		})
+	});
+  };
+  
   async function fetchTopTrackDates() {
     var numOlder = 0;
-    for (let i = 0; i < lenTopTracks; i++){
+    for (let i = 0; i < numTracks; i++){
       const response = await fetch(`https://api.spotify.com/v1/tracks/${topTracks[i]}`, {
         headers: {
           'Authorization': `Bearer ${spotifyToken}`
@@ -97,12 +154,16 @@ function App() {
     });  
     const json = await response.json();
     const topHitsIds = json.items;
-    const topHitsIdsLen = topHitsIds.length;
-    var listTopHits = [];
-    for (let i = 0; i < topHitsIdsLen; i++) {
-      listTopHits.push(topHitsIds[i].track.id)
+    try {
+      const topHitsIdsLen = topHitsIds.length;
+      var listTopHits = [];
+      for (let i = 0; i < topHitsIdsLen; i++) {
+        listTopHits.push(topHitsIds[i].track.id)
+      }
+      setTopHits(listTopHits);
+    } catch (error) {
     }
-    setTopHits(listTopHits);
+
   }
 
   function numTracksInTopHits() {
@@ -112,30 +173,12 @@ function App() {
     for (let user = 0; user < userTracksLen; user++){
       for (let top = 0; top < topTracksLen; top++) {
         if (topTracks[user] === topHits[top]) {
-          // console.log(topTracks[user])
           same.push(topTracks[user])
         } 
       }
     }
     return same.length;
   }
-
-
-
-  useEffect(() => {
-    
-    // spotifyApi.getPlaylistTracks({
-    //   playlistId: "37i9dQZF1DXcBWIGoYBM5M",
-    // }).then((response) => {
-    //   console.log(response)
-    //   setSongsInTopHits(response)
-    // })
-
-  }, [spotifyToken])
-
-  useEffect(() => {
-    getNowPlaying()
-  }, [])
 
   const getNowPlaying = () => {
     spotifyApi.getMyCurrentPlaybackState().then((response) => {
@@ -144,11 +187,6 @@ function App() {
         albumArt: response.item.album.images[0].url
       })
     })
-
-      // setNowPlaying({
-      //   name: response.items[0].name,
-      //   albumArt: response.items[0].album.images[0].url
-      // })
   }
 
   const getTopTracksID = () => {
@@ -159,7 +197,6 @@ function App() {
       setNumTracks(response.items.length)
       let tracksList = topTracks;
       for (let i = 0; i < numTracks; i++){
-        // console.log(response.items[i].name)
         let trackID = response.items[i].id
         if(tracksList.indexOf(trackID) === -1){
           tracksList.push(trackID); // song id 
@@ -167,26 +204,67 @@ function App() {
         
       }
       setTopTracks(tracksList);
-      setLenTopTracks(tracksList.length);
     })
   }
 
-  const getAllArtists = () => {
+  async function fetchAllArtists() {
+    getTopTracksID()
     let listenedArtists = artists;
     for (let i = 0; i < numTracks; i++){
-      spotifyApi.getTrack(topTracks[i]).then((response) => {
-        let artistURI = response.artists[0].uri.split(':')[2];
-        if(listenedArtists.indexOf(artistURI) === -1) {
-          listenedArtists.push(artistURI);
-        }      
-      })
-    }
+      const response = await fetch(`https://api.spotify.com/v1/tracks/${topTracks[i]}`, {
+        headers: {
+          'Authorization': `Bearer ${spotifyToken}`
+        }
+      }); 
+      const json = await response.json();  
+      const artistURI = json.artists[0].uri.split(':')[2];
+      if(listenedArtists.indexOf(artistURI) === -1) {
+        listenedArtists.push(artistURI);
+      }   
+    } 
     setArtists(listenedArtists);
     setNumArtists(listenedArtists.length)
   }
 
+  async function getAllArtists() {
+    await getTopTracksID()
+    let listenedArtists = artists;
+    if(loggedIn){
+      if (topTracks[0] === undefined) {
+        await getTopTracksID()
+      } else {
+        for (let i = 0; i < numTracks; i++){
+          spotifyApi.getTrack(topTracks[i]).then((response) => {
+            let artistURI = response.artists[0].uri.split(':')[2];
+            if(listenedArtists.indexOf(artistURI) === -1) {
+              listenedArtists.push(artistURI);
+            }      
+          }).catch(e => {
+          })
+        }
+        setArtists(listenedArtists);
+        setNumArtists(listenedArtists.length)
+      }
+    }
+  }
+
+  useEffect(() => {
+    getAllArtists()
+    console.log("here")
+  },[topTracks])
+
+  useEffect(() => {
+    console.log("numtracks", numTracks)
+  },[numTracks])
+  useEffect(() => {
+    console.log("artists length", numArtists)
+  },[numArtists])
+
   const getAllGenres = () => {
+	console.log("getting genres")
+	console.log(genres)
     let listenedGenres = genres; 
+	console.log(numArtists)
     for (let i = 0; i < numArtists; i++){
       spotifyApi.getArtist(artists[i]).then((response) => {
         let artistGenres = response.genres;
@@ -196,11 +274,22 @@ function App() {
             listenedGenres.push(currentGenre);
           }      
         }
+      }).catch(e => {
       })
     }
     setGenres(listenedGenres);
     setNumGenres(listenedGenres.length)
+	console.log("set genres", listenedGenres.length)
+	setPlaylistsAndGenres(listenedGenres.length+numArtists)
   }
+
+  useEffect(() => {
+	getAllGenres()
+  },[numArtists])
+
+  useEffect(() => {
+	calcVarietyScore()
+  },[genres])
 
   const getPlaylists = () => {
     let user = spotifyApi.getMe();
@@ -208,31 +297,50 @@ function App() {
       user_id: user,
       limit: 50
     }).then((response) => {
-      setNumPlaylists(response.total)
+      setNumPlaylists(response.items.length)
     }) 
   }
 
   
   // For playlist score:
   const calcPlaylistScore = () => {
+    getPlaylists()
     if (numPlaylists <= 10) {
       setPlaylistScore('M');
     } else {
-      setPlaylistScore('F');
+      setPlaylistScore('D');
     }
   }
 
+  useEffect(() => {
+    if (numPlaylists <= 10) {
+      setPlaylistScore('M');
+    } else {
+      setPlaylistScore('D');
+    }
+    // console.log(numPlaylists)
+  },[numPlaylists])
+
   // For variety score:
   const calcVarietyScore = () => {
-    // numArtists 
-    // numGenres
-    var score = numArtists + numGenres;
+    getAllArtists();
+    getAllGenres();
+    var score = playlistsAndGenres;
+	console.log("score", score)
     if (score <= 28) {
       setVarietyScore('L');
     } else {
       setVarietyScore('E');
     }
   }
+
+  useEffect(()=>{
+	console.log("variety score", varietyScore)
+	if(varietyScore > 0){
+		giveSpotifyMBTI()
+		setPersonality(playlistScore.concat(varietyScore, popularityScore, timeScore))
+	}
+  },[varietyScore])
 
   // For popularity score:
   const calcPopularityScore = () => {
@@ -255,39 +363,135 @@ function App() {
   }
 
   const giveSpotifyMBTI = () => {
+    fetchTopHitsIDs()
+    fetchTopTrackDates()
+    getTopTracksID()
+
     calcPlaylistScore()
     calcVarietyScore()
     calcPopularityScore()
     calcTimeScore()
+	console.log(numGenres)
     console.log(playlistScore, varietyScore, popularityScore, timeScore)
+    setPersonality(playlistScore.concat(varietyScore, popularityScore, timeScore))
   }
 
-  const createPlaylist = () => {
+  useEffect(() => {
+    console.log(personality)
+	if (personality.length === 4){
+		setPersonalityComplete(true)
+	}
+  },[personality])
 
-  }
+  // making playlist useeffect DON'T DELETE
+  useEffect(() => {
+	makePlaylist()
+  }, [newPlaylist])
 
+  useEffect(() => {
+	calcVarietyScore()
+	giveSpotifyMBTI()
+  }, [personality])
+
+//   useEffect(() => {
+// 	giveSpotifyMBTI()	
+//   }, [loggedIn])
+
+// loggedIn && has personality set 
   return (
     <div className="App">
-      {!loggedIn && <a href="http://localhost:8888">Login to Spotify</a>}
-      {loggedIn && (
-        <>
-          <div>Now Playing: {nowPlaying.name}</div>
-          <div>
-            <img src={nowPlaying.albumArt} style={{height: 150}}/>
-          </div>
-        </>
-      )}
-      {loggedIn && (
-        <div>
-          <button onClick={() => getTopTracksID()}>Tracks</button>
-          <button onClick={() => getAllArtists()}>Artists</button>
-          <button onClick={() => getAllGenres()}>Genres</button>
-          <button onClick={() => giveSpotifyMBTI()}>Get MBTI!</button>
-        </div>
-      )}
+      <Navbar />
+      {!loggedIn && <Home />}
+	  {loggedIn && !personalityComplete && 
+		<div className='loading-screen'>
+			<img src={"././loading.gif"} className="loading" alt="loading" />
+			<div className='loading-message'>
+				Calculating your listening personality...
+			</div>
+		</div>
+	  }
+	  {loggedIn && 
+	  <div>
+		{loggedIn && personalityComplete ?
+			<div>
+				<button onClick={() => giveSpotifyMBTI()}>Get MBTI!</button>
+				<div className='results'>
+					<div className='personality-side'>
+						<div className='personality-header'>
+							<div className='reg-personality-text'>
+								Your personality is  
+							</div>
+							<div className='header-acc-personality'>
+								&nbsp;{personality}
+							</div> 
+						</div>
+						<div className='personality-letters'>	
+							<div className='first-letter'>
+							{personality[0]}
+							<div className='letter-description'>
+								{personalities[personality[0]].slice(1)}
+							</div>
+							</div>
+							<div className='second-letter'>
+								{personality[1]}
+								<div className='letter-description'>
+									{personalities[personality[1]].slice(1)}
+								</div>
+							</div>
+							<div className='third-letter'>
+								{personality[2]}
+								<div className='letter-description'>
+									{personalities[personality[2]].slice(1)}
+								</div>
+							</div>
+							<div className='fourth-letter'>
+								{personality[3]}
+								<div className='letter-description'>
+									{personalities[personality[3]].slice(1)}
+								</div>
+							</div>
+						</div>
+						<div className='personality-desc'>
+							{descriptions['TEST']}
+						</div>
+						
+					</div>
+					<div className='playlist-side'>
+						<div className='make-playlist-section'>
+							<div className='make-playlist'>
+								Click
+							</div>
+							<div onClick={() => makePersonalityPlaylist()} className='make-playlist-link'>
+								&nbsp;here&nbsp;
+							</div>
+							<div className='make-playlist'>
+								for your personality playlist
+							</div>
+						</div>
+						<div className='player'>
+						<link
+							rel="preload"
+							href="https://open.spotifycdn.com/cdn/fonts/spoticon_regular_2.d728648c.woff2"
+							type="font/woff2"
+							crossOrigin="anonymous"
+						/>
+							<iframe src={`https://open.spotify.com/embed/track/${topTracks[0]}?utm_source=generator&theme=0`} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="encrypted-media" allowtransparency="true" loading="lazy"></iframe>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		: 
+			<div>
+				<button onClick={() => giveSpotifyMBTI()}>Get MBTI!</button>
+			</div>}
+		</div>
+		}
       {/* <h1>{track.name}</h1> */}
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
