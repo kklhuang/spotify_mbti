b2d3390365d99214fadb9fa2c119199475763f2f
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-04-19 23:08:14 -0400

    Update results page
    - made playlist creation to open playlist in Spotify 
    - updated with personality descriptions for each listening personality type

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,6 @@ function App() {
   const [newPlaylist, setNewPlaylist] = useState("");
   const [personalityComplete, setPersonalityComplete] = useState(false)
   const oldYearVar = 2003; 
- 
 
   // Personality scores 
   const [playlistScore, setPlaylistScore] = useState("");
@@ -56,7 +55,22 @@ function App() {
   }
 
   const descriptions = {
-	"TEST" : "Personality description hereLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+	"MLTP" : "You're a practical listener. Although you prefer music that is less mainstream, you still tend to enjoy music that's more contemporary than old-fashioned. You don't have too many playlists, and listen to the same type of music on a regular basis; so overall, you're a pretty low-maintenance listener. You know what you like, and you'll stick by it regardless of what's popular. ",
+	"MLTV" : "You're a listener that enjoys consistency and authenticity in their music. You really enjoy music from the past; either to relive your childhood and feel that sense of nostalgia, or you feel a strong desire to connect with that older time period. Consequently, you don't listen to as much mainstream music compared to the average listener. Overall, you'd be satisfied with your music as long as you have your minimal playlists and your regular artists.",
+	"METP" : "You're definitely a variation of what people can call the “average” music listener. Like many others, you're living in the present, and enjoy the music that's currently being put out. However, you stray away from the musical trends and don't usually gravitate towards what the rest of the world is listening to. You enjoy being slightly different from others, as you have a more diverse music taste and like to explore the various new music that's being released. You're able to summarize your music taste into a minimal amount of playlists, but you're content with what you have.",
+	"METV" : "In terms of music, you give off the impression that you were born in the wrong time period! Your music taste is very different from what most people like to listen to nowadays. You'd rather listen to music from older centuries, to either relive your childhood or connect with a certain time in history. To add, you listen to a wide variety of music too, showing your extensive appreciation for music outside of what is popular. You're able to summarize your music taste into a minimal amount of playlists, but you're content with what you have.",
+	"MLBP" : "You're what people may call the “average” music listener. You thoroughly enjoy music that most of the world enjoys. Whether the music you like is from the radio, or a trend on social media, popular music seems to frequently suit your music taste. Popular music in our times are usually from the same genres or artists, and you don't mind that at all. You also don't have too many playlists, and listen to the same type of music on a regular basis; so overall, you're a pretty low-maintenance listener.",
+	"MLBV" : "You're certainly a unique music listener! Not only are you a fan of what most of the world is currently listening to, but you also love a fair share of music from the pre-2000s. Despite this interesting difference, you still find comfort in the familiarity of the few genres and artists you listen to. Surprisingly, you're able to summarize your music taste into a minimal amount of playlists, but you're content with what you have.",
+	"MEBP" : "You may be somewhat of a variation off of the average music listener. You're certainly following the main trends as you listen to a lot of what's popular and what currently gets released. However, you also have a pretty diverse taste in music! You like the trends, but you like your fair share of what's not mainstream too. You like to have a little bit of everything, and you're also able to summarize that well with your minimal amount of playlists.",
+	"MEBV" : "You're a listener that certainly has more diversity in their music taste compared to the average listener! You listen to a large variety of music. Not only do you enjoy a good amount of music that comes from recent trends and from what's popular on the radio, but you also enjoy music from before the 2000s. You're well versed in the music world, seeing as you like so much music from so many different eras. Surprisingly, you're also able to summarize that well with your minimal amount of playlists.",
+	"DLTP" : "You're a music listener that knows what you like, and that will stick by it regardless of what's popular. You tend to stray away from the music that is seen as more mainstream, but you still enjoy other music that's from our current century. Regardless of what's being released, you stay loyal to your favourite artists. To accommodate your unique music taste, you create many playlists to fit all and any of your needs. ",
+	"DLTV" : "You're a listener that enjoys consistency and authenticity in their music. You really enjoy music from the past; either to relive your childhood and feel that sense of nostalgia, or you feel a strong desire to connect with that older time period. Consequently, you don't listen to as much mainstream music compared to the average listener. Since a lot of the modern music doesn't suit your taste, you've created countless playlists to be able to perfectly lay out your musical needs and personality.",
+	"DETP" : "You're definitely a variation of what people can call the “average” music listener. Like many others, you're living in the present, and enjoy the music that's currently being put out. However, you stray away from the musical trends and don't usually gravitate towards what the rest of the world is listening to. You enjoy being slightly different from others, as you have a more diverse music taste and like to explore the various new music that's being released. To accommodate for your unique music taste, you've made and saved multiple playlists that will suit your each and every mood, filled with all the different artists and genres you enjoy. You have an extremely diverse music taste, and the number of playlists you have certainly shows it!",
+	"DETV" : "You're the complete opposite of what people may call the “average” music listener. Your music taste is very different from what most people like to listen to nowadays. You'd rather listen to music from older centuries, to either relive your childhood or connect with a certain time in history. To add, you listen to a wide variety of music too, showing your extensive appreciation for music outside of what is popular. Seeing that your music taste is so diverse, it's fitting that you've also made and saved a large number of playlists to showcase that!",
+	"DLBP" : "You're a slight variation off what people may call the “average” music listener. You thoroughly enjoy music that most of the world enjoys. Whether the music you like is from the radio, or a trend on social media, popular music seems to frequently suit your music taste. Popular music in our times are usually from the same genres or artists, and you don't mind that at all. Uniquely enough, you have a lot of playlists to preserve the present.",
+	"DLBV" : "You're certainly a unique music listener! Not only are you a fan of what most of the world is currently listening to, but you also love a fair share of music from the pre-2000s. Despite this interesting difference, you still find comfort in the familiarity of the few genres and artists you listen to. Considering your music taste, it's fitting that you have so many playlists.",
+	"DEBP" : "You may be somewhat of a variation off of the average music listener. You're certainly following the main trends as you listen to a lot of what's popular and what currently gets released. However, you also have a pretty diverse taste in music! You like the trends, but you like your fair share of what's not mainstream too. You like to have a little bit of everything, and that diversity in your music taste is seen with your large amount of playlists.",
+	"DEBV" : "You're a listener that certainly has more diversity in their music taste compared to the average listener! You listen to a large variety of music. Not only do you enjoy a good amount of music that comes from recent trends and from what's popular on the radio, but you also enjoy music from before the 2000s. You're well versed in the music world, seeing as you like so much music from so many different eras. Considering your music taste, it's fitting that you have so many playlists.",
   }
 
   const [personality, setPersonality] = useState("");
@@ -84,14 +98,6 @@ function App() {
     })
   })
 
-//   async function makePersonalityPlaylist() {
-//       const response = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
-//         headers: {
-//           'Authorization': `Bearer ${spotifyToken}`
-//         }
-//       }); 
-//   }
-
   const makePersonalityPlaylist = async () => {
 	const response = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
 	  method: 'POST',
@@ -103,10 +109,14 @@ function App() {
 		name: personality + " Playlist",
 		description: "The playlist that created your Spotify Listener's Personality! Made by Karen Huang :)"
 	  })
+	  
 	});
-  
+
 	const data = await response.json();
 	setNewPlaylist(data.id)
+	const playlistURL = `https://open.spotify.com/playlist/${newPlaylist}?si=ad49785bb30f40ce`
+	window.open(playlistURL, "_blank");
+
 	return data.id; // return the new playlist ID
 
   };
@@ -452,20 +462,20 @@ function App() {
 							</div>
 						</div>
 						<div className='personality-desc'>
-							{descriptions['TEST']}
+							{descriptions[personality]}
 						</div>
 						
 					</div>
 					<div className='playlist-side'>
 						<div className='make-playlist-section'>
 							<div className='make-playlist'>
-								Click
+								Click&nbsp;
 							</div>
 							<div onClick={() => makePersonalityPlaylist()} className='make-playlist-link'>
-								&nbsp;here&nbsp;
+								here
 							</div>
 							<div className='make-playlist'>
-								for your personality playlist
+								&nbsp;for your personality playlist
 							</div>
 						</div>
 						<div className='player'>
@@ -487,7 +497,6 @@ function App() {
 			</div>}
 		</div>
 		}
-      {/* <h1>{track.name}</h1> */}
       <Footer />
     </div>
   );
