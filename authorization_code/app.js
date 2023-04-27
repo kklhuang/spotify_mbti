206b7b144eef87d06f6138a9ddf2e07d766f523e
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-04-27 15:54:07 -0400

    updating ports and urls

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -7,6 +7,8 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
+const port = process.env.PORT || 8888; 
+const domain = process.env.DOMAIN; 
 
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
@@ -16,7 +18,8 @@ var cookieParser = require('cookie-parser');
 
 var client_id = 'f10c5f99fc0b45fbbfd63f728bf2b040'; // Your client id
 var client_secret = 'a73db79db2c74e0f9f3ea021016fcc06'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+// var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+var redirect_uri = 'https://spotify-mbti.vercel.app/callback/?port=${port}'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -105,13 +108,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('https://spotify-mbti-frontend.vercel.app/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('https://spotify-mbti-frontend.vercel.app/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
@@ -145,4 +148,4 @@ app.get('/refresh_token', function(req, res) {
 });
 
 console.log('Listening on 8888');
-app.listen(8888);
+app.listen(port);
