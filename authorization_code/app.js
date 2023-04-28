ea93a4e4c2df907a8c5f8d12365fae3f92afeaa2
Karen Huang <56480921+kklhuang@users.noreply.github.com>
2023-04-28 11:24:26 -0400

    Update app.js

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -19,7 +19,7 @@ var cookieParser = require('cookie-parser');
 var client_id = 'f10c5f99fc0b45fbbfd63f728bf2b040'; // Your client id
 var client_secret = 'a73db79db2c74e0f9f3ea021016fcc06'; // Your secret
 // var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
-var redirect_uri = 'https://spotify-mbti.vercel.app/callback/'; // Your redirect uri
+var redirect_uri = 'https://spotify-mbti.vercel.app/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -148,4 +148,4 @@ app.get('/refresh_token', function(req, res) {
 });
 
 console.log('Listening on 8888');
-app.listen(port);
+// app.listen(port);
